2dc954afe8f08581727beed39013a2dffe53659d
Jaime Escobar <jaime.escobar.mtz@gmail.com>
2018-11-29 07:32:55 -0700

    Actual 29/11

--- src/controller/controller.java
+++ src/controller/controller.java
@@ -73,8 +73,8 @@ public class controller {
                     }
                     r.listResultados.setModel(modelo);
                     r.setVisible(true);
-                } catch (Exception E) {
-                    System.out.println(E.getMessage());
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
                 }
                 break;
             }
@@ -118,7 +118,7 @@ public class controller {
                         r.listResultados.setModel(modelo);
                         r.setVisible(true);
                     } else {
-                        JOptionPane.showMessageDialog(null, "Determinante es 0\n No tiene solucion aparente", "Aviso", JOptionPane.WARNING_MESSAGE);
+                        JOptionPane.showMessageDialog(null, "Determinante es 0\n>No tiene solucion aparente", "Aviso", JOptionPane.WARNING_MESSAGE);
                     }
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
@@ -138,6 +138,14 @@ public class controller {
                     System.out.println(e.getMessage());
                 }
 
+                break;
+            }
+            case 4: {
+                try {
+                    
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
                 break;
             }
             default:
